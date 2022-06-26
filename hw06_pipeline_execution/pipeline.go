f6bb9adb0b41283b0e9f9a915da263412b134127
Турченков Павел Александрович <turchenkov@ians.aero>
2022-06-26 22:52:57 +0400

    Значения высчитываются, но ловлю deadlock

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -1,5 +1,7 @@
 package hw06pipelineexecution
 
+import "sync"
+
 type (
 	In  = <-chan interface{}
 	Out = In
@@ -10,13 +12,23 @@ type Stage func(in In) (out Out)
 
 func ExecutePipeline(in In, done In, stages ...Stage) Out {
 	out := make(Bi)
-	temp := in
+	wg := sync.WaitGroup{}
 	go func() {
+		temp := in
 		defer close(out)
 		for _, s := range stages {
+			wg.Add(1)
+			defer wg.Done()
 			temp = s(temp)
 		}
-		out <- <-temp
+		go func() {
+			for t := range temp {
+				if t != nil {
+					out <- t
+				}
+			}
+		}()
+		wg.Wait()
 	}()
 
 	return out
