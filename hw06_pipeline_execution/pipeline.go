0f57c2160cad34de6a34085a9e4617f0b404ebd2
Турченков Павел Александрович <turchenkov@ians.aero>
2022-06-28 00:47:31 +0400

    return to old

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -10,16 +10,10 @@ type Stage func(in In) (out Out)
 
 func ExecutePipeline(in In, done In, stages ...Stage) Out {
 	out := make(Bi)
-	// test := make(chan struct{})
 	go func() {
 		for _, s := range stages {
-			select {
-			case <-done:
-				return
-			default:
-				s := s
-				in = s(in)
-			}
+			s := s
+			in = s(in)
 		}
 		go func() {
 			defer close(out)
