0bb158ca118f041efbd770367225155018cb5e21
Турченков Павел Александрович <turchenkov@ians.aero>
2022-06-27 16:30:11 +0400

    it's working!

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -1,7 +1,5 @@
 package hw06pipelineexecution
 
-import "sync"
-
 type (
 	In  = <-chan interface{}
 	Out = In
@@ -12,23 +10,24 @@ type Stage func(in In) (out Out)
 
 func ExecutePipeline(in In, done In, stages ...Stage) Out {
 	out := make(Bi)
-	wg := sync.WaitGroup{}
+	temp := in
 	go func() {
-		temp := in
-		defer close(out)
 		for _, s := range stages {
-			wg.Add(1)
-			defer wg.Done()
 			temp = s(temp)
 		}
 		go func() {
-			for t := range temp {
-				if t != nil {
-					out <- t
+			defer close(out)
+			for {
+				select {
+				case <-done:
+					return
+				case out <- <-temp:
+					continue
+				default:
+					return
 				}
 			}
 		}()
-		wg.Wait()
 	}()
 
 	return out
