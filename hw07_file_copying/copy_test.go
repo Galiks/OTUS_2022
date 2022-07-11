4f2fbb774e7ff588488ec7e01868d15f173fcdcb
Турченков Павел Александрович <turchenkov@ians.aero>
2022-07-11 13:22:37 +0400

    Немного тестов

--- hw07_file_copying/copy_test.go
+++ hw07_file_copying/copy_test.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"log"
 	"os"
 	"path/filepath"
 	"testing"
@@ -10,15 +9,45 @@ import (
 )
 
 func TestCopy(t *testing.T) {
-	// Place your code here.
-	dir, err := os.Getwd()
-	if err != nil {
-		log.Fatal(err)
-	}
-	destinationPath := filepath.Join(dir, "testoutput/foo/foo/foo/output.txt")
-	sourcePath := filepath.Join(dir, "testdata/input.txt")
-	err = Copy(sourcePath, destinationPath, 0, 6000)
-	require.Nil(t, err)
-	// err = os.Remove(destinationPath)
-	// require.Nil(t, err)
+
+	t.Run("full copy", func(t *testing.T) {
+		dir, err := os.Getwd()
+		require.Nil(t, err)
+		destinationPath := filepath.Join(dir, "testoutput/foo/foo/foo/output.txt")
+		sourcePath := filepath.Join(dir, "testdata/input.txt")
+		err = Copy(sourcePath, destinationPath, 0, 0)
+		require.Nil(t, err)
+		ds, err := os.Stat(destinationPath)
+		require.Nil(t, err)
+		ss, err := os.Stat(sourcePath)
+		require.Nil(t, err)
+		require.Equal(t, ds.Size(), ss.Size())
+		err = os.Remove(destinationPath)
+		require.Nil(t, err)
+	})
+
+	t.Run("copy with limit", func(t *testing.T) {
+		dir, err := os.Getwd()
+		require.Nil(t, err)
+		destinationPath := filepath.Join(dir, "testoutput/foo/foo/foo/output.txt")
+		sourcePath := filepath.Join(dir, "testdata/input.txt")
+		var destinationFileSize int64 = 1000
+		err = Copy(sourcePath, destinationPath, 0, destinationFileSize)
+		require.Nil(t, err)
+		ds, err := os.Stat(destinationPath)
+		require.Nil(t, err)
+		ss, err := os.Stat(sourcePath)
+		require.Nil(t, err)
+		require.Equal(t, ds.Size(), destinationFileSize)
+		require.NotEqual(t, ds.Size(), ss.Size())
+		// dest, err := os.OpenFile(destinationPath, os.O_RDONLY, 0644)
+		// require.Nil(t, err)
+		err = os.Remove(destinationPath)
+		require.Nil(t, err)
+	})
+
+	t.Run("copy with offset", func(t *testing.T) {
+
+	})
+
 }
