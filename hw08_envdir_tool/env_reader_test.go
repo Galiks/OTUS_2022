e32790acad4b9055a2e0eb9cabd6b587bb440d99
Турченков Павел Александрович <turchenkov@ians.aero>
2022-07-17 22:59:39 +0400

    test on ReadDir

--- hw08_envdir_tool/env_reader_test.go
+++ hw08_envdir_tool/env_reader_test.go
@@ -1,7 +1,27 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"path/filepath"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
 
 func TestReadDir(t *testing.T) {
-	// Place your code here
+	t.Run("simple test", func(t *testing.T) {
+		expectedLen := 5
+		dir, err := os.Getwd()
+		require.Nil(t, err)
+		dirPath := filepath.Join(dir, "/testdata/env")
+		env, err := ReadDir(dirPath)
+		require.Nil(t, err)
+		require.Equal(t, expectedLen, len(env))
+	})
+
+	t.Run("invalid dir", func(t *testing.T) {
+		env, err := ReadDir("")
+		require.NotNil(t, err)
+		require.Nil(t, env)
+	})
 }
