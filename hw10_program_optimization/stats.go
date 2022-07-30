ab2cbb6e0e3f14c7e0c629695034d39b0f9228cf
Pavel Turchenkov <31892748+Galiks@users.noreply.github.com>
2022-07-30 04:35:28 +0400

    Update stats.go

--- hw10_program_optimization/stats.go
+++ hw10_program_optimization/stats.go
@@ -1,106 +1,106 @@
-package hw10programoptimization
+	package hw10programoptimization
 
-import (
-	"bufio"
-	"encoding/json"
-	"errors"
-	"fmt"
-	"github.com/buger/jsonparser"
-	"io"
-	"io/ioutil"
-	"regexp"
-	"strings"
-)
+	import (
+		"bufio"
+		"encoding/json"
+		"errors"
+		"fmt"
+		"github.com/buger/jsonparser"
+		"io"
+		"io/ioutil"
+		"regexp"
+		"strings"
+	)
 
-type User struct {
-	ID       int
-	Name     string
-	Username string
-	Email    string
-	Phone    string
-	Password string
-	Address  string
-}
-
-var (
-	ErrReaderIsNil   = errors.New("reader is nil")
-	ErrDomainIsEmpty = errors.New("domain is empty")
-)
-
-type DomainStat map[string]int
-
-func GetDomainStat(r io.Reader, domain string) (DomainStat, error) {
-	if r == nil {
-		return nil, ErrReaderIsNil
+	type User struct {
+		ID       int
+		Name     string
+		Username string
+		Email    string
+		Phone    string
+		Password string
+		Address  string
 	}
 
-	if domain == "" {
-		return nil, ErrDomainIsEmpty
-	}
-	bf := bufio.NewReader(r)
-	stats := make(DomainStat)
+	var (
+		ErrReaderIsNil   = errors.New("reader is nil")
+		ErrDomainIsEmpty = errors.New("domain is empty")
+	)
 
-	for {
-		line, _, err := bf.ReadLine()
-		if err != nil {
-			if errors.Is(err, io.EOF) {
-				break
+	type DomainStat map[string]int
+
+	func GetDomainStat(r io.Reader, domain string) (DomainStat, error) {
+		if r == nil {
+			return nil, ErrReaderIsNil
+		}
+
+		if domain == "" {
+			return nil, ErrDomainIsEmpty
+		}
+		bf := bufio.NewReader(r)
+		stats := make(DomainStat)
+
+		for {
+			line, _, err := bf.ReadLine()
+			if err != nil {
+				if errors.Is(err, io.EOF) {
+					break
+				}
+				return nil, err
 			}
-			return nil, err
+			email, err := jsonparser.GetUnsafeString(line, "Email")
+			if err != nil {
+				return nil, err
+			}
+			if strings.HasSuffix(email, domain) {
+				emailDomain := email[strings.IndexRune(email, '@')+1:]
+				stats[strings.ToLower(emailDomain)]++
+			}
 		}
-		email, err := jsonparser.GetUnsafeString(line, "Email")
+		return stats, nil
+	}
+
+	func GetDomainStatOld(r io.Reader, domain string) (DomainStat, error) {
+		u, err := getUsers(r)
 		if err != nil {
-			return nil, err
+			return nil, fmt.Errorf("get users error: %w", err)
 		}
-		if strings.HasSuffix(email, domain) {
-			emailDomain := email[strings.IndexRune(email, '@')+1:]
-			stats[emailDomain]++
+		return countDomains(u, domain)
+	}
+
+	type users [100_000]User
+
+	func getUsers(r io.Reader) (result users, err error) {
+		content, err := ioutil.ReadAll(r)
+		if err != nil {
+			return
 		}
-	}
-	return stats, nil
-}
 
-func GetDomainStatOld(r io.Reader, domain string) (DomainStat, error) {
-	u, err := getUsers(r)
-	if err != nil {
-		return nil, fmt.Errorf("get users error: %w", err)
-	}
-	return countDomains(u, domain)
-}
-
-type users [100_000]User
-
-func getUsers(r io.Reader) (result users, err error) {
-	content, err := ioutil.ReadAll(r)
-	if err != nil {
+		lines := strings.Split(string(content), "\n")
+		for i, line := range lines {
+			var user User
+			if err = json.Unmarshal([]byte(line), &user); err != nil {
+				return
+			}
+			result[i] = user
+		}
 		return
 	}
 
-	lines := strings.Split(string(content), "\n")
-	for i, line := range lines {
-		var user User
-		if err = json.Unmarshal([]byte(line), &user); err != nil {
-			return
+	func countDomains(u users, domain string) (DomainStat, error) {
+		result := make(DomainStat)
+
+		for _, user := range u {
+			matched, err := regexp.Match("\\."+domain, []byte(user.Email))
+			if err != nil {
+				return nil, err
+			}
+
+			if matched {
+				num := result[strings.ToLower(strings.SplitN(user.Email, "@", 2)[1])]
+				num++
+				result[strings.ToLower(strings.SplitN(user.Email, "@", 2)[1])] = num
+			}
 		}
-		result[i] = user
+		return result, nil
 	}
-	return
-}
-
-func countDomains(u users, domain string) (DomainStat, error) {
-	result := make(DomainStat)
-
-	for _, user := range u {
-		matched, err := regexp.Match("\\."+domain, []byte(user.Email))
-		if err != nil {
-			return nil, err
-		}
-
-		if matched {
-			num := result[strings.ToLower(strings.SplitN(user.Email, "@", 2)[1])]
-			num++
-			result[strings.ToLower(strings.SplitN(user.Email, "@", 2)[1])] = num
-		}
-	}
-	return result, nil
-}
